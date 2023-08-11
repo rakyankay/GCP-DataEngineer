317ca6f29c788e5f15152ab9ce9519ec71e58587
rakyankay <rakyankay@gmail.com>
2023-08-11 17:22:30 +0700

    Update Table BigQuery Name

--- Quest-DataProcessing/Quest-3-Streaming-Pipeline/output.sh
+++ Quest-DataProcessing/Quest-3-Streaming-Pipeline/output.sh
@@ -1,2 +1,5 @@
-bq head logs.raw
-bq head logs.windowed_traffic
+#Check Streaming Data on Big Query
+bq head logs.raw_streaming
+
+#Check Windowed Streaming on BigQuery
+bq head logs.windowed_minute_traffic
